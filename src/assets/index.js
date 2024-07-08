2fc00302c600b358876d141dd9e4b0e9f5d7bb80
elfill03 <rafaelfillah755@gmail.com>
2024-07-08 09:03:25 +0700

    fix bug

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,4 +4,3 @@ export { default as img2 } from "./img/image 2.png";
 export { default as img3 } from "./img/image 3.png";
 export { default as img5 } from "./img/image 4.png";
 export { default as img6 } from "./img/login-image.png";
-export { default as img4 } from "./img/profile.jpg";
