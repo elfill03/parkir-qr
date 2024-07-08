3063baf3d256494ebe7a49a3199206ecd8cec341
elfill03 <rafaelfillah755@gmail.com>
2024-07-08 09:49:40 +0700

    Fix bcryptjs import issue

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['bcryptjs']
-    },
-    outDir: 'dist' // Pastikan output ke direktori 'dist'
+      // Hapus external configuration
+    }
   }
 })
